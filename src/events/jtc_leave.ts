3a1577ec9d34fc6fd4a80ac5465a71761b4c28b5
Sush1sui <jplovesraw@gmail.com>
2025-04-13 22:09:41 +0800

    removed auto owner if user is only one left in vc

--- src/events/jtc_leave.ts
+++ src/events/jtc_leave.ts
@@ -29,44 +29,46 @@ export default {
             await channel.delete();
 
             console.log(`Removed ${channel.id} from database`);
-          } else if (channel.members.size === 1) {
-            // Get the remaining member
-            const remainingMember = channel.members.first();
-            if (!remainingMember) return;
-
-            // Check if they are not already the owner
-            const oldOwner = oldState.member;
-            if (
-              oldOwner &&
-              channel
-                .permissionsFor(oldOwner)
-                ?.has(PermissionFlagsBits.ManageChannels) &&
-              oldOwner.id !== remainingMember.id
-            ) {
-              console.log(
-                `${oldOwner.user.tag} left, transferring ownership to ${remainingMember.user.tag}`
-              );
-
-              // Remove ManageChannels and MoveMembers permission from the old owner
-              await channel.permissionOverwrites.edit(oldOwner.id, {
-                ManageChannels: false,
-                MoveMembers: false,
-              });
-
-              // Grant ManageChannels and MoveMembers permission to the new owner
-              await channel.permissionOverwrites.edit(remainingMember.id, {
-                ManageChannels: true,
-                MoveMembers: true,
-              });
-
-              // Update the database with the new owner
-              await changeOwnerCustomVC(channel.id, remainingMember.id);
-
-              console.log(
-                `Ownership transferred to ${remainingMember.user.tag}`
-              );
-            }
           }
+
+          // else if (channel.members.size === 1) {
+          //   // Get the remaining member
+          //   const remainingMember = channel.members.first();
+          //   if (!remainingMember) return;
+
+          //   // Check if they are not already the owner
+          //   const oldOwner = oldState.member;
+          //   if (
+          //     oldOwner &&
+          //     channel
+          //       .permissionsFor(oldOwner)
+          //       ?.has(PermissionFlagsBits.ManageChannels) &&
+          //     oldOwner.id !== remainingMember.id
+          //   ) {
+          //     console.log(
+          //       `${oldOwner.user.tag} left, transferring ownership to ${remainingMember.user.tag}`
+          //     );
+
+          //     // Remove ManageChannels and MoveMembers permission from the old owner
+          //     await channel.permissionOverwrites.edit(oldOwner.id, {
+          //       ManageChannels: false,
+          //       MoveMembers: false,
+          //     });
+
+          //     // Grant ManageChannels and MoveMembers permission to the new owner
+          //     await channel.permissionOverwrites.edit(remainingMember.id, {
+          //       ManageChannels: true,
+          //       MoveMembers: true,
+          //     });
+
+          //     // Update the database with the new owner
+          //     await changeOwnerCustomVC(channel.id, remainingMember.id);
+
+          //     console.log(
+          //       `Ownership transferred to ${remainingMember.user.tag}`
+          //     );
+          //   }
+          // }
         }
       }
     } catch (error) {
